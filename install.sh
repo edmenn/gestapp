fc98beec6e933930e2fa8d9ccbc80414b75d922d
DevTechpy <115493707+DevTechpy@users.noreply.github.com>
2022-10-20 10:52:29 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apt install docker 
+apt install docker-compose
+
+cp installer/.env /var/www/html/
+mv * /var/www/html/
+
+chmod 777 -R /var/www/html/
+
+cd /var/www/html/
+
+docker run --rm -v  $(pwd):/app composer install
+
+
+cd /var/www/html/installer/
+
+ls -la
+
+docker-compose up --build -d
+
+docker exec -ti php php artisan migrate
+docker exec -ti php php artisan db:seed
+docker exec -ti php php artisan key:generate
+docker exec -ti php php -S localhost:8000 server.php 
+
+
